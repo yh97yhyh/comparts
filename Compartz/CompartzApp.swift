19985662aaeab14029bd3091e9911714425eb397
영현 <yh97yhyh@naver.com>
2024-03-02 01:32:22 +0900

    first commit

---
+++ Compartz/CompartzApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompartzApp.swift
+//  Compartz
+//
+//  Created by 영현 on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CompartzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
